aa30452f6443f2cecc43412ed0adfcdb1c5c42e5
xilibi2003 <xilibi2003@163.com>
2024-02-17 12:45:38 +0800

    add script for create account

---
+++ create_by_mnemonic.js
@@ -0,0 +1,21 @@
+var bip39 = require('bip39')
+var hdkey = require('ethereumjs-wallet/hdkey')
+var util = require('ethereumjs-util')
+
+// 生成助记词
+var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"; // bip39.generateMnemonic()
+console.log("助记词："+ mnemonic );
+
+// var seed = bip39.mnemonicToSeed(mnemonic)
+var seed = bip39.mnemonicToSeed(mnemonic);
+var hdWallet = hdkey.fromMasterSeed(seed);
+
+var key1 = hdWallet.derivePath("m/44'/60'/0'/0/1");
+console.log("私钥："+util.bufferToHex(key1._hdkey._privateKey));
+
+var address1 = util.pubToAddress(key1._hdkey._publicKey, true);
+console.log("地址："+util.bufferToHex(address1));
+
+
+
+console.log("校验和地址："+ util.toChecksumAddress(address1.toString('hex')));
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Readme
+
+1. create eth account by random private key 
+
+```
+node create_by_raw.js 
+```
+
+2. create ethereum account by random  mnemonic
+
+```
+node create_by_mnemonic.js
+```
